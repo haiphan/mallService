c0d04eb0bcc7855bb45a7490fd7750faeb3633ee
haiphan <hai.phan@zoho.com>
2019-03-31 00:25:42 +0200

    Default error handler

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,21 @@ import {ShoppingCenterController, StoreController, OfferController} from './cont
 const {PORT} = process.env;
 const port: number = PORT ? Number(PORT) : 3000;
 const app: express.Application = express();
+
+const defaultErrHandler = (req: express.Request, res: express.Response) =>
+  res.status(400).json({
+    message: 'Failed to process request'
+  });
+const routes = express.Router();
+routes.use('/', defaultErrHandler);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/stores', StoreController);
 app.use('/offers', OfferController);
 app.use('/shoppingcenter', ShoppingCenterController);
+app.use(routes);
 
 app.listen(port, () =>
   console.log(`Listening at http://localhost:${port}/`)
